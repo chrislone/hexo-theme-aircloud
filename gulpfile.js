3fb3bdb09c0f557062d2479d2497cc4c6c853afe
chrislone <chriislonee@gmail.com>
2019-10-07 19:21:33 +0800

    使用 gulp 进行构建

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,46 @@
 const gulpLess = require('gulp-less')
 const path = require('path')
-const gulp = require('gulp')
+const { src, dest, task } = require('gulp')
+const concatCss = require('gulp-concat-css')
+const cleanCSS = require('gulp-clean-css')
 
-gulp.task('less', function() {
-  return gulp
-    .src('./source/_less/**/*.less')
+const cssConcatSequence = [
+  'source/_less/common.css',
+  'source/_less/layout.css',
+  'source/_less/variables.css',
+  'source/_less/nav.css',
+  'source/_less/index.css',
+  'source/_less/page.css',
+  'source/_less/post.css',
+  'source/_less/theme.css',
+  'source/_less/tag.css',
+  'source/_less/archive.css',
+  'source/_less/about.css',
+  'source/_less/diff.css',
+  'source/_less/toc.css',
+  'source/_less/hightlight.css',
+  'source/_less/gitment.css',
+  'source/_less/_partial/footer.css',
+  'source/_less/_partial/donate.css'
+]
+
+task('css', function() {
+  return (
+    src(cssConcatSequence, {
+      allowEmpty: true
+    })
+      .pipe(concatCss('./source/css/aircloud.css'))
+      // .pipe(cleanCSS())
+      .pipe(dest('.'))
+  )
+})
+
+task('less', function() {
+  return src('./source/_less/**/*.less')
     .pipe(
       gulpLess({
         paths: [path.join(__dirname, 'source/_less')]
       })
     )
-    .pipe(gulp.dest('./source/css/aircloud.css'))
+    .pipe(dest('./source/_less/'))
 })
